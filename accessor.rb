660d9b225ead18a925dd82d48e9a5e8c6e676224
souravxw <codersourav245@gmail.com>
2024-12-05 20:11:56 +0530

    using attr_accessor and attr_reader

--- accessor.rb
+++ accessor.rb
@@ -21,3 +21,36 @@ person.name = "Alice"
 puts person.name 
 person.name = "Proloy"
 puts person.name
+
+
+# using attr_accessor 
+class Animal
+  # getter and setter for name and age
+  attr_accessor :name, :age  
+  # only a getter for type (read-only) 
+  attr_reader :type           
+
+  def initialize(name, age, type)
+    @name = name
+    @age = age
+    @type = type
+  end
+end
+
+
+animal = Animal.new("Bella", 3, "Dog")
+
+
+puts animal.name  
+puts animal.age   
+puts animal.type 
+
+
+animal.name = "Max"
+animal.age = 4
+
+
+puts animal.name  
+puts animal.age   
+
+
